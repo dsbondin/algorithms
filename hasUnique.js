adb84831412d034f42f218352edbaae6894582f2
github username <dsbondin@gmail.com>
2018-03-08 20:43:40 -0500

    hasUnique, hasUniqueLoop

---
+++ hasUnique.js
@@ -0,0 +1,27 @@
+// Implement an algorithm to determine if a string has all unique characters.
+// What if you can not use additional data structures?
+
+
+function hasUnique(string) {
+  let hash = {}
+  for (i = 0; i < string.length; i++ ) {
+    if (!hash[string[i]]) {
+      hash[string[i]] = 1
+    } else {
+      return true
+    }
+  }
+  return false
+}
+
+function hasUniqueLoop(string) {
+  for (i = 0; i < string.length - 1; i++ ) {
+    let char = string[i];
+    for (j = i + 1; j < string.length; j++) {
+      if (string[j] === char) {
+        return true
+      }
+    }
+  }
+  return false
+}
