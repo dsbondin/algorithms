7f5a4a4a9c7a9c116fea51ca26a5c42e4fa7c659
github username <dsbondin@gmail.com>
2018-03-13 21:34:19 -0400

    reverseWordsInString

--- reverseString.js
+++ reverseString.js
@@ -4,6 +4,19 @@ function reverseStringRecursively(string) {
   } else if (string.length > 1){
     return reverseStringRecursively(string.slice(1)) + string[0]
   } else {
-    return false;
+    return '';
   }
+}
+
+function reverseStringIteratively(string) {
+  let reversedString = ''
+  const l = string.length - 1
+  for (let i = l; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  return reversedString;
+}
+
+function reverseWordsInString(string) {
+  return string.split(' ').map(word => reverseStringRecursively(word)).join(' ');
 }
