9d29a35fa73902f2657ef286a635e47a653d21aa
github username <dsbondin@gmail.com>
2018-03-14 21:27:30 -0400

    linkedList in js

---
+++ linkedList.js
@@ -0,0 +1,14 @@
+// implement linked list in js
+
+class Node {
+  constructor(value, next) {
+    this.value = value;
+    this.next = next
+  }
+}
+
+let z = new Node("z", null)
+let y = new Node("y", z)
+let x = new Node("z", y)
+x.next.next.value
+// 'z'
