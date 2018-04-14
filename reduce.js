b9aa0092e974608c152c84ed687bb368deb0306d
github username <dsbondin@gmail.com>
2018-04-14 09:12:15 -0400

    reduce.js

--- reduce.js
+++ reduce.js
@@ -1,3 +1,42 @@
+[1, 2, 3, 4].reduce((prev, next) => {
+  console.log(prev, next)
+  return prev + next;
+}, 0)
+
+// 0 1
+// 1 2
+// 3 3
+// 6 4
+// => 10
+
+[1, 2, 3, 4].reduce((prev, next) => {
+  console.log(prev, next);
+  if (next % 2 === 0) prev.push(next);
+  return prev;
+}, [])
+
+// [] 1
+// [] 2
+// [ 2 ] 3
+// [ 2 ] 4
+// => [ 2, 4 ]
+
+const weather = {
+  "New York": 65,
+  "Washington, DC": 67,
+  "Miami": 78,
+  "San Francisco": 62
+}
+
+const hottest = Object.keys(weather)
+  .reduce((prev, next) => {
+    return weather[prev] > weather[next] ? prev : next;
+  });
+
+console.log(hottest)
+
+// Miami
+
 const products = ['Apple, iPhone, $699',
                   'Apple, iMac, $1899',
                   'Apple, airPods, $159',
@@ -7,10 +46,10 @@ const products = ['Apple, iPhone, $699',
 
 let output = products
   .map(line => line.split(', '))
-  .reduce((line, string) => {
-    line[string[0]] = line[string[0]] || [];
-    line[string[0]].push({product: string[1], price: string[2]})
-    return line;
+  .reduce((manufacturer, product) => {
+    manufacturer[product[0]] = manufacturer[product[0]] || [];
+    manufacturer[product[0]].push({productName: product[1], price: product[2]})
+    return manufacturer;
   }, {});
 
 console.log(JSON.stringify(output, null, 2));
