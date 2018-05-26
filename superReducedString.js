3cd6adefc9b20374b5b28a30f357b5f0ca68aff0
github username <dsbondin@gmail.com>
2018-05-26 09:36:04 -0400

    superReducedString

---
+++ superReducedString.js
@@ -0,0 +1,23 @@
+function superReducedString(s) {
+    function remove(str) {
+        for (let i = 0; i < str.length - 1; i++) {
+            if (str[i] === str[i+1]) {
+                return str.slice(0, i) + str.slice(i+2)
+            }
+        }
+        return str;
+    }
+
+    while (s !== remove(s)) {
+        s = remove(s)
+    }
+
+    if(s === '') return 'Empty String'
+    return s;
+}
+
+const abcoocba = superReducedString('abcoocba');
+const abbcccddeee = superReducedString('abbcccddeee');
+
+console.log(abcoocba); // Empty String
+console.log(abbcccddeee); // ace
