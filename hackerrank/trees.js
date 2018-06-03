7ff3d86620b84e04fc88adae0cf86328154d5e4c
github username <dsbondin@gmail.com>
2018-06-03 09:45:24 -0400

    fix dfs

--- hackerrank/trees.js
+++ hackerrank/trees.js
@@ -1,5 +1,5 @@
-const Node = (value) => {
-  this.value = value;
+function Node(data) {
+  this.data = data;
   this.left = null;
   this.right = null;
 }
@@ -7,8 +7,9 @@ const Node = (value) => {
 // Depth-first Search
 const dfs = (root, value) => {
   if (root.data === value) return true;
-  if (root.left) dfs(root.left, value);
-  if (root.right) dfs(root.right, value);
+  if (root.left || root.right) {
+    return dfs(root.left, value) || dfs(root.right, value);
+  }
   return false;
 }
 
@@ -23,3 +24,16 @@ const bfs = (root, value) => {
   }
   return false;
 }
+
+let root = new Node(3);
+root.left = new Node(5);
+root.right = new Node(7);
+root.left.left = new Node(9);
+root.left.right = new Node(11);
+root.right.left = new Node(13);
+root.right.right = new Node(15);
+
+console.log('dfs 13: ', dfs(root, 11));
+console.log('bfs 13: ', bfs(root, 11));
+console.log('dfs 12: ', dfs(root, 12));
+console.log('bfs 12: ', bfs(root, 12));
