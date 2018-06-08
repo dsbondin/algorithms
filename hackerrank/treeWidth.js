5d40db18a97e2fb44befb1eb7976c85675ec2111
github username <dsbondin@gmail.com>
2018-06-08 18:29:35 -0400

    maxTreeWidth, leftNodes

--- hackerrank/treeWidth.js
+++ hackerrank/treeWidth.js
@@ -4,15 +4,10 @@ function Node(data) {
   this.right = null;
 }
 
-let root = new Node(7);
-root.left = new Node(5);
-root.right = new Node(9);
-root.left.left = new Node(4);
-root.left.right = new Node(6);
-root.right.left = new Node(8);
-root.right.right = new Node(10);
+// return binary tree width at each level
 
 const treeWidth = (root) => {
+  // width at level 0 is 0 (root is level 1)
   let widths = [0];
   let queue = [root];
   while (queue.length !== 0) {
@@ -27,4 +22,46 @@ const treeWidth = (root) => {
   return widths;
 }
 
+const maxTreeWidth = (root) => {
+  let maxWidth = 0;
+  let queue = [root];
+  while (queue.length !== 0) {
+    let l = queue.length;
+    if (l > maxWidth) maxWidth = l;
+    for (let i = 0; i < l; i++) {
+      let node = queue.shift();
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+  return maxWidth;
+}
+
+const leftNodes = (root) => {
+  let lNodes = [];
+  let queue = [root];
+  while (queue.length !== 0) {
+    lNodes.push(queue[0].data);
+    let l = queue.length;
+    for (let i = 0; i < l; i++) {
+      let node = queue.shift();
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+  }
+  return lNodes;
+}
+
+let root = new Node(7);
+root.left = new Node(5);
+root.right = new Node(9);
+root.left.left = new Node(4);
+root.left.right = new Node(6);
+root.right.left = new Node(8);
+root.right.right = new Node(10);
+root.right.right.right = new Node(12);
+root.right.right.left = new Node(15);
+
 console.log(treeWidth(root));
+console.log(maxTreeWidth(root));
+console.log(leftNodes(root));
